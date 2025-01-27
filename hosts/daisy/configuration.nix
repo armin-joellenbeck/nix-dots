9cc8b6f95d4e9b6e0a9f5f24d6c4ae705e62335e
Armin Jöllenbeck <armin@joellenbeck.net>
2025-01-27 14:10:23 +0100

    Fix: nvidia modsettings true is required

--- hosts/daisy/configuration.nix
+++ hosts/daisy/configuration.nix
@@ -184,14 +184,14 @@
 
     # Modesetting is required.
     # TODO: Default is false
-    # modesetting.enable = true;
+    modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
     # of just the bare essentials.
     # TODO: Default is false
-    #powerManagement.enable = false;
+    # powerManagement.enable = true;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
